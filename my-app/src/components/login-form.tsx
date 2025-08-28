398717df21e25f9c9c7068b0231d119d78b4a8b4
Rekhta Menahil <rekhta674@gmail.com>
2025-08-28 21:03:19 +0500

    add auth POST login endpoint

--- my-app/src/components/login-form.tsx
+++ my-app/src/components/login-form.tsx
@@ -1,30 +1,62 @@
+"use client"
 import { cn } from "@/lib/utils"
 import { Button } from "@/components/ui/button"
 import {
   Card,
   CardContent,
-  CardDescription,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
+import { useMutation } from "@tanstack/react-query"
+import { login } from "../http/api"
+import { useState } from "react"
+import { email } from "zod"
+import { LoginFormValues, useAuthStore } from "@/store"
+import { useRouter } from "next/navigation"
+
 
 export function LoginForm({
   className,
   ...props
 }: React.ComponentProps<"div">) {
+  const [loginData, setLoginData] = useState<LoginFormValues>({
+    email: '',
+    password: ''
+  });
+
+//  const LoginPage = ()=>{
+  const {setUser} = useAuthStore()
+  const router = useRouter();
+
+  
+  const {mutate}=useMutation({
+    mutationKey: ['login'],
+    mutationFn: login,
+    onSuccess: (userData)=>{
+      setUser(userData.data);
+      router.push('/dashboard')
+    },
+    onError: (error) => {
+         console.error('Login failed:', error);
+        }
+      })
+  
+  const handleLogin = (e: React.FormEvent)=>{
+    e.preventDefault()
+    mutate(loginData)
+  }
   return (
     <div className={cn("flex flex-col gap-6", className)} {...props}>
       <Card>
         <CardHeader>
           <CardTitle>Login</CardTitle>
-          <CardDescription>
-            Enter your email below to login to your account
-          </CardDescription>
         </CardHeader>
         <CardContent>
-          <form>
+          <form
+          onSubmit={handleLogin}
+          >
             <div className="flex flex-col gap-6">
               <div className="grid gap-3">
                 <Label htmlFor="email">Email</Label>
@@ -33,6 +65,8 @@ export function LoginForm({
                   type="email"
                   placeholder="m@example.com"
                   required
+                  value={loginData.email}
+                  onChange={(e)=> setLoginData({...loginData, email: e.target.value})}
                 />
               </div>
               <div className="grid gap-3">
@@ -45,22 +79,16 @@ export function LoginForm({
                     Forgot your password?
                   </a>
                 </div>
-                <Input id="password" type="password" required />
+                <Input id="password" type="password" required 
+                value={loginData.password}
+                onChange={(e)=> setLoginData({...loginData, password: e.target.value})}
+                />
               </div>
               <div className="flex flex-col gap-3">
                 <Button type="submit" className="w-full">
                   Login
                 </Button>
-                <Button variant="outline" className="w-full">
-                  Login with Google
-                </Button>
               </div>
-            </div>
-            <div className="mt-4 text-center text-sm">
-              Don&apos;t have an account?{" "}
-              <a href="#" className="underline underline-offset-4">
-                Sign up
-              </a>
             </div>
           </form>
         </CardContent>
@@ -68,3 +96,5 @@ export function LoginForm({
     </div>
   )
 }
+
+  
