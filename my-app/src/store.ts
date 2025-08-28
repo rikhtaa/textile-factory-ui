75cafca8ea335806f70d24edb405a26b4ba51a80
Rekhta Menahil <rekhta674@gmail.com>
2025-08-28 20:53:38 +0500

    add auth store

---
+++ my-app/src/store.ts
@@ -0,0 +1,36 @@
+import { create } from "zustand"
+import { devtools  } from "zustand/middleware"
+
+export interface Worker {
+  name: string;
+  phone: number;
+  role: string;
+  email: string;
+  status: string;
+  hireDate: Date;
+  password: string;
+}
+
+export interface LoginFormValues {
+  email: string
+  password: string
+}
+
+export type WorkerCredentails={
+    email: string
+    password: string
+}
+
+interface AuthState{
+    user: null | LoginFormValues
+    setUser: (user: LoginFormValues)=> void
+    logout: ()=> void   
+}
+
+export const useAuthStore = create<AuthState>()(
+   devtools((set)=>({
+    user: null,
+    setUser: (user)=> set({user}),
+    logout: ()=> set({user: null})
+   }))
+)
