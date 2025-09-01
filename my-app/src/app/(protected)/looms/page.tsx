edac2f4dd8fc9ddfe04ce6f63a3201084336de17
Rekhta Menahil <rekhta674@gmail.com>
2025-09-01 12:55:32 +0500

    fix loom heading

--- my-app/src/app/(protected)/looms/page.tsx
+++ my-app/src/app/(protected)/looms/page.tsx
@@ -26,7 +26,7 @@ export default function LoomsPage(){
 return (
         <div className='w-full flex items-center justify-center overflow-hidden'>
              <div className='flex flex-col gap-7 w-[85%] py-[1rem]'>
-               <h2 className='text-black text-4xl font-bold'>Qualities</h2>
+               <h2 className='text-black text-4xl font-bold'>Looms</h2>
                 <AddLoom onFormSubmit={handleFormData}/>
                 <LoomTable  looms={loomsData?.data || []}/>
             </div>
