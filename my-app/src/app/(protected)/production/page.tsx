77538ee519af06dee999037f56163ff9291c9cc3
Rekhta Menahil <rekhta674@gmail.com>
2025-10-04 15:17:05 +0500

    fix remaining beam

--- my-app/src/app/(protected)/production/page.tsx
+++ my-app/src/app/(protected)/production/page.tsx
@@ -1,25 +1,31 @@
 'use client'
 import { AddProduction } from '@/components/addProduction'
 import { FilterType, ProductionTable } from '@/components/productionTable';
-import { createProduction, getListProduction } from '@/http/api';
+import { createProduction, getAllBeams, getListProduction } from '@/http/api';
 import { ApiErrorResponse, CreateProduction } from '@/store';
-import { useMutation, useQuery } from '@tanstack/react-query';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 import { AxiosError } from 'axios';
 import React, { useState } from 'react'
 import { toast } from 'sonner';
 export default function Page() {
+  const queryClient = useQueryClient()
   const [filters, setFilters] = useState<FilterType>({
       date: "",
       loomId: "",
       factoryId: "",
-      operatorId: ""
+      operatorId: "",
+      beamId: "",
+      qualityId: "",
     })
+  const { data: beamsData } = useQuery({ queryKey: ['beam'], queryFn: getAllBeams })  
   const {mutate: userMutate, error, isSuccess, isPending} = useMutation({
       mutationKey: ['produciton'],
       mutationFn: createProduction, 
       onSuccess: ()=>{
            refetch()
             toast.success("User has been created");
+            queryClient.invalidateQueries({queryKey: ['beam']})      
+            queryClient.invalidateQueries({queryKey: ['produciton']}) 
       },
      onError: (error: AxiosError<ApiErrorResponse>) => {
       if (error.response) {
@@ -51,13 +57,15 @@ export default function Page() {
      <div className='w-full flex items-center justify-center overflow-hidden'>
           <div className='flex flex-col gap-7 w-[85%] py-[1rem]'>
             <h2 className='text-black text-4xl font-bold'>Production</h2>
-            <AddProduction onFormSubmit={handleFormData}  onSucess={isSuccess} isPending={isPending} />
+            <AddProduction beamsData={beamsData?.data || []}   onFormSubmit={handleFormData}  onSucess={isSuccess} isPending={isPending} />
             <ProductionTable 
             filters={filters}
             setFilters={setFilters}
             productionData={productionData?.data || []} 
             isLoading={isLoading}
-            error={error} />
+            error={error}
+            beamsData={beamsData?.data || []} 
+            />
          </div>
           </div>
   )
