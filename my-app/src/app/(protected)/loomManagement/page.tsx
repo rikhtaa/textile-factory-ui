4a8031bfe02680be753d989621c322863e48de42
Rekhta Menahil <rekhta674@gmail.com>
2025-10-08 23:56:54 +0500

    separate  ComboBox

--- my-app/src/app/(protected)/loomManagement/page.tsx
+++ my-app/src/app/(protected)/loomManagement/page.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { CustomCombobox } from "@/components/addProduction";
+import { CustomCombobox } from "../../../components/customCombobox";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
---
+++ my-app/src/components/customCombobox.tsx
@@ -0,0 +1,58 @@
+import { ChevronsUpDownIcon } from "lucide-react"
+import { Command, CommandEmpty, CommandGroup, CommandInput, CommandItem, CommandList } from "@/components/ui/command"
+import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover"
+import { Button } from "./ui/button";
+
+export const CustomCombobox = ({
+  value,
+  onValueChange,
+  items,
+  placeholder,
+  open,
+  onOpenChange
+}: {
+  value: string;
+  onValueChange: (value: string) => void
+  items: { value: string; label: string }[];
+  placeholder: string;
+  open: boolean;
+  onOpenChange: (open: boolean) => void;
+}) => (
+  <Popover open={open} onOpenChange={onOpenChange}>
+    <PopoverTrigger asChild>
+      <Button
+        variant="outline"
+        role="combobox"
+        aria-expanded={open}
+        className="w-full justify-between"
+      >
+        {value
+          ? items.find((item) => item.value === value)?.label
+          : placeholder}
+        <ChevronsUpDownIcon className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+      </Button>
+    </PopoverTrigger>
+    <PopoverContent className="w-full p-0">
+      <Command>
+        <CommandInput placeholder={`Search ${placeholder.toLowerCase()}...`} />
+        <CommandList>
+          <CommandEmpty>No results found.</CommandEmpty>
+          <CommandGroup>
+            {items?.map((item) => (
+              <CommandItem
+                key={item.value}
+                value={item.value}
+                onSelect={(currentValue) => {
+                  onValueChange(currentValue === value ? "" : currentValue)
+                  onOpenChange(false)
+                }}
+              >
+                {item.label}
+              </CommandItem>
+            ))}
+          </CommandGroup>
+        </CommandList>
+      </Command>
+    </PopoverContent>
+  </Popover>
+)
